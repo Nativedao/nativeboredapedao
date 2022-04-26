268919f73c33cca3215344cb0c3c7e129a1142dc
Nativedao <104433646+Nativedao@users.noreply.github.com>
2022-04-26 13:35:16 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Native Bored Ape DAO!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Native Bored Ape DAO!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          The native Passport of Native Metaverse <code>pages/index.js</code>
         </p>
       </main>
 
